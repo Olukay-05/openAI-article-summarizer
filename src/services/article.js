071f028b6123c4f59500c47a0c116edef0cf0f38
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2023-04-24 20:40:26 +0100

    Added .env file

--- src/services/article.js
+++ src/services/article.js
@@ -1,28 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-const axios = require('axios');
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-const options = {
-  method: 'GET',
-  url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
-  params: {
-    url: 'https://time.com/6266679/musk-ai-open-letter/',
-    length: '3'
-  },
-  headers: {
-    'content-type': 'application/octet-stream',
-    'X-RapidAPI-Key': 'f5d0787476mshcf5408c3f1e5465p15a84bjsne094acf7e22a',
-    'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
-  }
-};
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
 
 
 export const articleApi = createApi({
@@ -30,7 +11,7 @@ export const articleApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', 'KEY');
+      headers.set('X-RapidAPI-Key', rapidApiKey);
       headers.set('X-RapidAPI-Host',
       'article-extractor-and-summarizer.p.rapidapi.com');
     }
